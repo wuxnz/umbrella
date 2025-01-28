986c35b3195893c53762434fa47c008d50f5a4b0
woop <damnedsinner206517@gmail.com>
2025-01-27 20:34:37 -0600

    Fixed ScrollView on PluginInfoView page.

--- src/features/plugins/presentation/views/PluginInfoView.tsx
+++ src/features/plugins/presentation/views/PluginInfoView.tsx
@@ -23,124 +23,127 @@ const PluginInfoView = ({route}: {route: any}) => {
   const navigation = useNavigation();
 
   return (
-    <ScrollView
-      contentContainerStyle={{flexGrow: 1}}
-      showsVerticalScrollIndicator={false}
-      style={{
-        ...styles.container,
-        backgroundColor:
-          colorScheme === 'dark'
-            ? DarkTheme.colors.background
-            : LightTheme.colors.background,
-      }}>
-      <Appbar.Header>
-        <Appbar.BackAction
-          onPress={() => {
-            navigation.goBack();
-          }}
-        />
-        <Appbar.Content title={plugin.name} />
-        <Appbar.Action icon="star" onPress={() => {}} />
-        <Appbar.Action icon="earth" onPress={() => {}} />
-        <Appbar.Action icon="share" onPress={() => {}} />
-      </Appbar.Header>
-      <View />
-      {plugin.bannerImageUrl !== undefined ? (
-        <>
-          <ImageBackground
-            source={{uri: plugin.bannerImageUrl}}
-            style={styles.banner}>
-            <LinearGradient
-              colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.85)']}
-              style={{flex: 1}}></LinearGradient>
-          </ImageBackground>
-        </>
-      ) : (
-        <View style={styles.bannerPlaceholderContainer}>
-          <Icon source="power-plug" size={50} />
-        </View>
-      )}
-      <View style={styles.pluginInfoContainer}>
-        {plugin.iconUrl !== undefined ? (
-          <Image
-            source={{uri: plugin.iconUrl}}
-            style={styles.authorAvatarImage}
+    <View style={{flex: 1}}>
+      <ScrollView
+        contentContainerStyle={{flexGrow: 1}}
+        showsVerticalScrollIndicator={false}
+        style={{
+          ...styles.scrollView,
+          backgroundColor:
+            colorScheme === 'dark'
+              ? DarkTheme.colors.background
+              : LightTheme.colors.background,
+        }}>
+        <Appbar.Header>
+          <Appbar.BackAction
+            onPress={() => {
+              navigation.goBack();
+            }}
           />
+          <Appbar.Content title={plugin.name} />
+          <Appbar.Action icon="star" onPress={() => {}} />
+          <Appbar.Action icon="earth" onPress={() => {}} />
+          <Appbar.Action icon="share" onPress={() => {}} />
+        </Appbar.Header>
+        {plugin.bannerImageUrl !== undefined ? (
+          <>
+            <ImageBackground
+              source={{uri: plugin.bannerImageUrl}}
+              style={styles.banner}>
+              <LinearGradient
+                colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.85)']}
+                style={{flex: 1}}></LinearGradient>
+            </ImageBackground>
+          </>
         ) : (
-          <View style={styles.authorAvatarIcon}>
-            <Icon source="account" size={50} />
+          <View style={styles.bannerPlaceholderContainer}>
+            <Icon source="power-plug" size={50} />
           </View>
         )}
-        <View style={styles.pluginNameAndDescription}>
-          <Text variant="headlineMedium" style={{fontWeight: 'bold'}}>
-            {plugin.name}
-          </Text>
-          <Text variant="bodyMedium">{plugin.author}</Text>
-          <Text variant="bodyMedium">{plugin.description}</Text>
+        <View style={styles.pluginInfoView}>
+          <View style={styles.pluginInfoContainer}>
+            {plugin.iconUrl !== undefined ? (
+              <Image
+                source={{uri: plugin.iconUrl}}
+                style={styles.authorAvatarImage}
+              />
+            ) : (
+              <View style={styles.authorAvatarIcon}>
+                <Icon source="account" size={50} />
+              </View>
+            )}
+            <View style={styles.pluginNameAndDescription}>
+              <Text variant="headlineMedium" style={{fontWeight: 'bold'}}>
+                {plugin.name}
+              </Text>
+              <Text variant="bodyMedium">{plugin.author}</Text>
+              <Text variant="bodyMedium">{plugin.description}</Text>
+            </View>
+            <View style={{flex: 1}} />
+          </View>
+          <View style={styles.pluginMetaDataContainer}>
+            <Chip style={styles.chip}>Version: {plugin.version}</Chip>
+            {plugin.homePageUrl !== undefined ? (
+              <Chip
+                closeIcon={'open-in-new'}
+                style={styles.chip}
+                onClose={() => {
+                  Linking.openURL(plugin.homePageUrl);
+                }}>
+                Open Source
+              </Chip>
+            ) : null}
+            <Chip
+              closeIcon={'open-in-new'}
+              style={styles.chip}
+              onClose={() => {
+                Linking.openURL(plugin.manifestUrl);
+              }}>
+              Manifest
+            </Chip>
+            <Chip
+              closeIcon={'open-in-new'}
+              style={styles.chip}
+              onClose={() => {
+                Linking.openURL(plugin.pluginUrl);
+              }}>
+              Plugin
+            </Chip>
+          </View>
+          <List.Section style={styles.pluginDetailsContainer}>
+            <List.Item
+              title="Changelog"
+              description={plugin.changelog}
+              onPress={() => {}}
+            />
+            <List.Item
+              title="Readme"
+              description={plugin.readme}
+              onPress={() => {}}
+            />
+            <List.Item
+              title="License"
+              description={plugin.license}
+              onPress={() => {}}
+            />
+            <List.Item
+              title="Manifest File Location"
+              description={plugin.manifestPath}
+              onPress={() => {
+                FileViewer.open(plugin.manifestPath);
+              }}
+            />
+            <List.Item
+              title="Plugin File Location"
+              description={plugin.pluginPath}
+              onPress={() => {
+                FileViewer.open(plugin.pluginPath);
+              }}
+            />
+          </List.Section>
         </View>
-        <View style={{flex: 1}} />
-      </View>
-      <View style={styles.pluginMetaDataContainer}>
-        <Chip style={styles.chip}>Version: {plugin.version}</Chip>
-        {plugin.homePageUrl !== undefined ? (
-          <Chip
-            closeIcon={'open-in-new'}
-            style={styles.chip}
-            onClose={() => {
-              Linking.openURL(plugin.homePageUrl);
-            }}>
-            Open Source
-          </Chip>
-        ) : null}
-        <Chip
-          closeIcon={'open-in-new'}
-          style={styles.chip}
-          onClose={() => {
-            Linking.openURL(plugin.manifestUrl);
-          }}>
-          Manifest
-        </Chip>
-        <Chip
-          closeIcon={'open-in-new'}
-          style={styles.chip}
-          onClose={() => {
-            Linking.openURL(plugin.pluginUrl);
-          }}>
-          Plugin
-        </Chip>
-      </View>
-      <List.Section style={styles.pluginDetailsContainer}>
-        <List.Item
-          title="Changelog"
-          description={plugin.changelog}
-          onPress={() => {}}
-        />
-        <List.Item
-          title="Readme"
-          description={plugin.readme}
-          onPress={() => {}}
-        />
-        <List.Item
-          title="License"
-          description={plugin.license}
-          onPress={() => {}}
-        />
-        <List.Item
-          title="Manifest File Location"
-          description={plugin.manifestPath}
-          onPress={() => {
-            FileViewer.open(plugin.manifestPath);
-          }}
-        />
-        <List.Item
-          title="Plugin File Location"
-          description={plugin.pluginPath}
-          onPress={() => {
-            FileViewer.open(plugin.pluginPath);
-          }}
-        />
-      </List.Section>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 };
 
@@ -149,7 +152,12 @@ export default PluginInfoView;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '100%',
+  },
+  scrollView: {
+    flex: 1,
+  },
+  pluginInfoView: {
+    marginTop: -50,
   },
   banner: {
     width: '100%',
@@ -161,8 +169,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'gray',
   },
   pluginInfoContainer: {
-    position: 'absolute',
-    top: 210,
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 8,
@@ -181,10 +187,8 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
   pluginMetaDataContainer: {
-    position: 'absolute',
     flex: 1,
     width: '100%',
-    top: 320,
     paddingVertical: 8,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
@@ -200,9 +204,7 @@ const styles = StyleSheet.create({
         : Dimensions.get('window').width / 2 - 24,
   },
   pluginDetailsContainer: {
-    position: 'absolute',
     flex: 1,
     width: '100%',
-    top: 412,
   },
 });
